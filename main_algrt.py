8c57ab0a6898e14a2c666324b1a203abc3be947e
Ruslan Alimov <2001.ruslan.alimov@gmail.com>
2022-09-06 00:59:07 +0300

    Rewrite code in OOP format
    At the beginning of the project, functionality of program was small and I really didn't think about extending. But after 1 week of coding I have added few more functions, and It became problematic to extend the program in non-OOP code format.
    
    - All main window code was rewritten to OOP code format.
    - Parsing thread now using user_folder_path to download images on PC.
    - "Открыть Папку с файлами" now is opening folder which was chosen by user in folder browser.
    - "resource_path" function was relocated to 'options.py' module for non-repeatable code in modules.

--- main_algrt.py
+++ main_algrt.py
@@ -1,113 +1,112 @@
+import subprocess
 import tkinter as tk
 from tkinter import END
 from tkinter import filedialog
 
 from GIF_Animation_CLASS import ImageLabel
 from Parsing_machine import ParseMachine
-from options import keys
+import options
 
-import os
-import sys
 import threading
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+class MainWindow(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.iconbitmap(options.resource_path('Icon.ico'))
+        self.title("Парсер Фотографий товара из WB")
+        self.geometry('717x600+100+50')
+        self.resizable(False, False)
 
-    return os.path.join(base_path, relative_path)
+        self.user_folder_path = ''
+        self.answer_widgets = []
 
+        window_header = tk.Label(self, height=2, text="Парсер WILDBERRIES", bg='purple', fg='white', font=("Arial", 15))
+        window_header.pack(anchor=tk.NW, padx=8, fill=tk.X)
 
-def remove_last_answers():
-    """
-    Функция удаляет все виджеты со старыми ответами на прошлые ссылки.
-    :return:
-    """
-    for widg in answer_widgets:
-        widg.destroy()
+        choose_folder_label = tk.Label(self, height=1, text="Папка для сохранения фотографий:")
+        choose_folder_label.pack(anchor=tk.NW, padx=8)
 
+        folder_browser_frame = tk.Frame(self)
+        self.new_folder_path = tk.StringVar()
+        self.folder_path_label = tk.Label(folder_browser_frame, justify=tk.LEFT, width=86, height=1,
+                                          text="Не указано...", bg='white',  borderwidth=1, relief=tk.SUNKEN)
+        self.folder_path_label.pack(side=tk.LEFT, padx=7)
+        choose_folder_btn = tk.Button(folder_browser_frame, text="Выбрать ...", width=9,
+                                      command=self.folder_browse_button)
+        choose_folder_btn.pack(side=tk.LEFT)
+        folder_browser_frame.pack(anchor=tk.W, padx=5)
 
-def start_parsing():
-    remove_last_answers()
-    last_link = e.get("1.0", "end-1c")
-    last_link_label.config(text="Поиск по ссылке: " + last_link)
-    parser = ParseMachine()
-    th = threading.Thread(target=parser.search_imgs,
-                          args=(last_link, answer_widgets, answers_frame, loading_gif_label, search_btn),
-                          name="name")
-    th.start()
-    e.delete("1.0", END)  # очищаем запрос
+        insert_web_link_label = tk.Label(self, height=1, text="Ссылка на страницу товара WB:")
+        insert_web_link_label.pack(anchor=tk.NW, padx=8)
 
+        f_search = tk.Frame(self)
+        self.wb_link_entry = tk.Text(f_search, height=1, width=75)
+        self.wb_link_entry.pack(side=tk.LEFT)
+        self.wb_link_entry.bind("<Control-KeyPress>", options.keys)
+        self.search_btn = tk.Button(f_search, width=9, text='Поиск', command=self.start_parsing)
+        self.search_btn.pack(side=tk.LEFT, padx=10)
+        f_search.pack(anchor=tk.NW, padx=10)
 
-answer_widgets = []
+        self.last_link_label = tk.Label(self, height=3)
+        self.last_link_label.pack(anchor=tk.W, padx=20)
 
-master = tk.Tk()
-master.iconbitmap(resource_path('Icon.ico'))
-master.title("Парсер Фотографий товара из WB")
-master.geometry('717x600+100+50')
-master.resizable(False, False)
+        answers_links_listbox_canvas = tk.Canvas(self, borderwidth=0, background="#ffffff")
+        self.answers_frame = tk.Frame(answers_links_listbox_canvas, background="#ffffff")
+        self.answers_frame.bind("<Configure>",
+                                lambda event, canvas=answers_links_listbox_canvas: canvas.configure(scrollregion=canvas.bbox("all")))
+        vsb = tk.Scrollbar(answers_links_listbox_canvas, orient="vertical", command=answers_links_listbox_canvas.yview)
+        answers_links_listbox_canvas.configure(yscrollcommand=vsb.set)
+        vsb.pack(side="right", fill="y")
+        answers_links_listbox_canvas.pack(anchor=tk.W, fill=tk.BOTH, expand=True, padx=10)
+        answers_links_listbox_canvas.create_window((4, 4), window=self.answers_frame, anchor="nw")
 
+        self.loading_gif_label = ImageLabel(self.answers_frame, bg='white')
+        self.loading_gif_label.load(options.resource_path('loading.gif'))
+        self.loading_gif_label.pack_forget()
 
-tk.Label(master, height=2, text="Парсер WILDBERRIES", bg='purple', fg='white', font=("Arial", 15)).pack(anchor=tk.NW, padx=8, fill=tk.X)
+        self.open_in_folder_btn = tk.Button(self, width=20, text='Открыть в папке', command=self.open_user_folder)
+        self.open_in_folder_btn['state'] = tk.DISABLED
+        self.open_in_folder_btn.pack(side=tk.LEFT, anchor=tk.SW, padx=15, pady=12)
+        tk.Button(self, width=9, text='Выход', command=self.destroy).pack(side=tk.RIGHT, anchor=tk.SE, padx=15, pady=12)
 
+    def start_parsing(self):
+        self.remove_last_answers()
+        last_link = self.wb_link_entry.get("1.0", "end-1c")
+        self.last_link_label.config(text="Поиск по ссылке: " + last_link)
 
-def browse_button():
-    # Allow user to select a directory and store it in global var
-    # called folder_path
-    global folder_path
-    filename = filedialog.askdirectory()
-    folder_path.set(filename)
-    print(filename)
+        parser = ParseMachine()
+        th = threading.Thread(target=parser.search_imgs,
+                              args=(last_link, self.answer_widgets, self.user_folder_path, self.answers_frame,
+                                    self.loading_gif_label, self.search_btn), name="name")
+        th.start()
 
+        self.wb_link_entry.delete("1.0", END)  # очищаем запрос
 
-tk.Label(master, height=1, text="Папка для сохранения фотографий:").pack(anchor=tk.NW, padx=8)
+    def remove_last_answers(self):
+        """
+        Функция удаляет все виджеты со старыми ответами на прошлые ссылки.
+        :return:
+        """
+        for widg in self.answer_widgets:
+            widg.destroy()
 
-folder_browser_frame = tk.Frame(master)
-folder_path = tk.StringVar()
-folder_path_label = tk.Label(folder_browser_frame, width=86, height=1, textvariable=folder_path, justify=tk.LEFT,
-                             bg='white', borderwidth=1, relief=tk.SUNKEN)
-folder_path_label.pack(side=tk.LEFT, padx=7)
-button2 = tk.Button(folder_browser_frame, text="Выбрать ...", width=9, command=browse_button)
-button2.pack(side=tk.LEFT)
-folder_browser_frame.pack(anchor=tk.W, padx=5)
+    def folder_browse_button(self):
+        # Allow user to select a directory and store it in global var
+        # called folder_path
+        filename = filedialog.askdirectory()
+        self.new_folder_path.set(filename)
+        if filename != '':
+            self.folder_path_label.config(text=filename)
+            self.user_folder_path = filename
+            # Разблокируем кнопку "Показать в папке" если указан путь к файлу.
+            self.open_in_folder_btn['state'] = tk.NORMAL
 
-tk.Label(master, height=1, text="Ссылка на страницу товара WB:").pack(anchor=tk.NW, padx=8)
-
-
-f_search = tk.Frame(master)
-e = tk.Text(f_search, height=1, width=75)
-e.pack(side=tk.LEFT)
-e.bind("<Control-KeyPress>", keys)
-
-search_btn = tk.Button(f_search, width=9, text='Поиск', command=start_parsing)
-search_btn.pack(side=tk.LEFT, padx=10)
-f_search.pack(anchor=tk.NW, padx=10)
-
-last_link_label = tk.Label(master, height=3)
-last_link_label.pack(anchor=tk.W, padx=20)
-
-canvas = tk.Canvas(master, borderwidth=0, background="#ffffff")
-answers_frame = tk.Frame(canvas, background="#ffffff")
-vsb = tk.Scrollbar(canvas, orient="vertical", command=canvas.yview)
-canvas.configure(yscrollcommand=vsb.set)
-vsb.pack(side="right", fill="y")
-canvas.pack(anchor=tk.W, fill=tk.BOTH, expand=True, padx=10)
-canvas.create_window((4, 4), window=answers_frame, anchor="nw")
-answers_frame.bind("<Configure>", lambda event, canvas=canvas: canvas.configure(scrollregion=canvas.bbox("all")))
-
-loading_gif_label = ImageLabel(answers_frame, bg='white')
-loading_gif_label.pack()
-loading_gif_label.load(resource_path('loading.gif'))
-loading_gif_label.pack_forget()
-
-open_in_folder_btn = tk.Button(master, width=20, text='Открыть в папке', command=None)
-open_in_folder_btn['state'] = tk.DISABLED
-open_in_folder_btn.pack(side=tk.LEFT, anchor=tk.SW, padx=15, pady=12)
-tk.Button(master, width=9, text='Выход', command=master.destroy).pack(side=tk.RIGHT, anchor=tk.SE, padx=15, pady=12)
+    def open_user_folder(self):
+        folder_path = self.user_folder_path.replace('/', '\\')
+        subprocess.Popen(f'explorer /open, "{folder_path}"')
 
 
-master.mainloop()
+if __name__ == '__main__':
+    main_window = MainWindow()
+    main_window.mainloop()
