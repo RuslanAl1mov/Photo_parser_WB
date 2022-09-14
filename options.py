c899bd7ebdd0897a3996c59500743eeba3b86ac2
Ruslan Alimov <2001.ruslan.alimov@gmail.com>
2022-09-14 18:03:28 +0300

    Add function for copy loaded images to buffer.
    With creating new function for downloading photos from website we need to update already downloaded photos to new photos. Also in new parser update we have function for coping downloaded files in buffer.
    
    Changes:
    - For updating last downloaded photos to new photos, was created function "clear_last_images", which delete all last downloaded photos, before starting parser.
     - To clear downloaded cache was created function called "clear_cache_folder", which clears cache folder.
     - "files_to_clipboard" function was created for copy downloaded files in buffer for future comfortable usability of loaded files.

--- options.py
+++ options.py
@@ -1,6 +1,10 @@
 import ctypes
 import sys
 import os
+from ctypes import wintypes
+import win32clipboard
+
+import pythoncom
 
 
 def is_ru_lang_keyboard(a):
@@ -17,7 +21,7 @@ def is_ru_lang_keyboard(a):
 def keys(event):
     """
     Варианты нажатия на кнопки Копировать, Вставить, Вырезать, Очистить и Выбрать всё. Так как Tkinter не дает
-    возможности пользоваться горячими клавишами при русской расскладке клавиатуры, нам необходимо проверять на
+    возможности пользоваться горячими клавишами при русской раскладке клавиатуры, нам необходимо проверять на
     "НИЗКОМ" уровне какие кнопки были нажаты. И в зависимости от вида кнопки, выполнять определенные функции.
     :param event:
     :return:
@@ -47,3 +51,78 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+
+def clear_last_images(folder_path: str):
+    """
+    Функция очистки последних скаченных картинок в папку указанную пользователем. Функция выделяет фотографии с фразой
+    "wbimg" и разрешением ".jpg", выделяет и удаляет фотографии.
+    :param folder_path: Путь к папке указанный пользователем.
+    :return:
+    """
+    folder_path = folder_path.replace("\\", "/")
+    folder_list = os.listdir(folder_path)
+    for file_name in folder_list:
+        if 'wbimg' in file_name and '.jpg' in file_name:
+            path = os.path.join(folder_path, file_name)
+            try:
+                os.remove(path)
+            except FileNotFoundError as e:
+                print("clear_last_images ==> " + repr(e))
+
+
+def clear_cache_folder(folder_path: str):
+    """
+    Функция очищения файлов из кеш-папки. Программа очищает все что есть в папке указанной в "folder_path"
+    :param folder_path: Путь к кеш-папке.
+    :return:
+    """
+    folder_path = folder_path.replace("\\", "/")
+    folder_list = os.listdir(folder_path)
+    for file_name in folder_list:
+        path = os.path.join(folder_path, file_name)
+        try:
+            os.remove(path)
+        except FileNotFoundError as e:
+            print("clear_cache_folder ==> " + repr(e))
+
+
+class DROPFILES(ctypes.Structure):
+    _fields_ = (('pFiles', wintypes.DWORD),
+                ('pt', wintypes.POINT),
+                ('fNC', wintypes.BOOL),
+                ('fWide', wintypes.BOOL))
+
+
+def files_to_clipboard(files_path: str):
+    """
+    Функция для добавления файлов в Буфер обмена. Функция копирует скаченные фотографии в Буфер обмена,
+    для последующего их использования.  Функция копирует фотографии с фразой "wbimg" и разрешением ".jpg".
+    :param files_path: Путь к папке, откуда требуется копировать файлы.
+    :return:
+    """
+    files_list = []
+    for file in os.listdir(files_path):
+        if 'wbimg' in file and '.jpg' in file:
+            files_list.append(os.path.join(files_path, file))
+
+    offset = ctypes.sizeof(DROPFILES)
+    length = sum(len(p) + 1 for p in files_list) + 1
+    size = offset + length * ctypes.sizeof(ctypes.c_wchar)
+    buf = (ctypes.c_char * size)()
+
+    df = DROPFILES.from_buffer(buf)
+    df.pFiles, df.fWide = offset, True
+    for path in files_list:
+        array_t = ctypes.c_wchar * (len(path) + 1)
+        path_buf = array_t.from_buffer(buf, offset)
+        path_buf.value = path
+        offset += ctypes.sizeof(path_buf)
+    stg = pythoncom.STGMEDIUM()
+    stg.set(pythoncom.TYMED_HGLOBAL, buf)
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    try:
+        win32clipboard.SetClipboardData(win32clipboard.CF_HDROP, stg.data)
+    finally:
+        win32clipboard.CloseClipboard()
